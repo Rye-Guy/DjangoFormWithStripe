d9c8d1b15bbbf0f5036e379f5414963b9bc53343
Rye-Guy <ryanracioppo95@gmail.com>
2018-12-04 16:02:21 -0500

    added extra fields to form

--- form/forms.py
+++ form/forms.py
@@ -54,16 +54,43 @@ class PaymentForm(forms.Form):
         ('option5', 'Internet Access')
     )
 
+    TORONTO_BOOTH_OPTIONS = (
+        ('booth', 'All Booths - $1995')
+    )
+
+    CALGARY_BOOTH_OPTIONS = (
+        ('booth1', 'Platinum - $2995'),
+        ('booth2', 'Gold - $2495'),
+        ('booth3', 'Silver - $1995'),
+        ('booth4', 'Bronze - $1495')
+    )
+    EDMONTON_BOOTH_OPTIONS = (
+        ('booth1', 'Platinum - $2995'),
+        ('booth2', 'Gold - $2495'),
+        ('booth3', 'Silver - $1995'),
+        ('booth4', 'Bronze - $1495')
+    )
+    WINNIPEG_BOOTH_OPTIONS = (
+        ('booth1', 'Platinum - $2995'),
+        ('booth2', 'Gold - $2495'),
+        ('booth3', 'Silver - $1995'),
+        ('booth4', 'Bronze - $1495')
+    )
+
 
 
     select_cities = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=CITY_CHOICES)
     toronto_dates = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=TORONTO_DATES)
+    toronto_booth_options = forms.BooleanField(label='All Booths - $1995')
     toronto_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=TORONTO_FAIR_OPTIONS)
     calgary_dates = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=CALGARY_DATES)
+    calgary_booth_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=CALGARY_BOOTH_OPTIONS)
     calgary_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=CALGARY_FAIR_OPTIONS)
     edmonton_dates = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=EDMONTON_DATES)
+    edmonton_booth_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=EDMONTON_BOOTH_OPTIONS)
     edmonton_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                             choices=EDMONTON_FAIR_OPTIONS)
     winnipeg_dates = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=WINNIPEG_DATES)
+    winnipeg_booth_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=WINNIPEG_BOOTH_OPTIONS)
     winnipeg_options = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                                         choices=WINNIPEG_FAIR_OPTIONS)
