96edc586db49aa4ce8d31a86bfdd46432d0ef23d
JamalHam <hamilton.jamal.anthony@gmail.com>
2018-12-18 12:52:59 -0500

    views update

--- form/views.py
+++ form/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.http import HttpResponseRedirect
@@ -5,12 +6,16 @@ from django.http import HttpResponseRedirect
 
 from form.forms import PaymentForm, CustomerModelFormClass
 
+import datetime
+import stripe
+
 class IndexPageView(TemplateView):
     template_name = 'base-html.html'
     formPayment = PaymentForm()
     form_class = PaymentForm
     formContactInfo = CustomerModelFormClass()
     some_text = "YEAH! PUT SOME TEXT IN ME!!!"
+    stripeApiKey = settings.STRIPE_SECRET_KEY
 
     def get_context_data(self, **kwargs):
         context = super(IndexPageView, self).get_context_data(**kwargs)
@@ -20,12 +25,18 @@ class IndexPageView(TemplateView):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         print(request.POST)
-        print(request.POST.get('price', ''))
+        price = request.POST.get('price', '')
         if form.has_error('select_cities'):
             form.add_error(self, form.select_cities)
         if form.is_valid():
             print('valid')
-            return HttpResponseRedirect('/')
+            charge = stripe.Charge.create(
+                amount=price,
+                currency='cad',
+                description='Django stripe charge',
+                source=request.POST['stripeToken']
+            )
+            return HttpResponseRedirect('/payment.html')
 
         return render(request, self.template_name, {'form': form})
 
