61bd76a4d9c6aa0fa40715d101f6492c4518a41d
Rye-Guy <ryanracioppo95@gmail.com>
2019-01-02 17:00:18 -0500

    fixed up css for text inputs and urls

--- form/static/js/animationTiggers.js
+++ form/static/js/animationTiggers.js
@@ -6,7 +6,32 @@ billingInfoContainer = document.getElementById('billingInfoContainer')
 billingInfoContainer.className = 'hide-me'
 
 billingInfoBtn.addEventListener('click', (ev)=>{
-    ev.preventDefault()
+    //ev.preventDefault()
     console.log('Yo yo ma')
     billingInfoContainer.className = 'animated fadeInLeft'
+})
+
+allInputs = document.querySelectorAll("input[type='text']")
+allUrlInputs = document.querySelectorAll("input[type='url']")
+
+function helperTextCheck(input){
+    input.addEventListener('blur', (ev)=>{
+        console.log(input, ev)
+        if(input.value == "" || input.value.length < 0){
+            console.log('leave helper text')
+        }
+        else{
+            console.log(input.nextSibling)
+            input.nextSibling.className = 'helperTextRemain'
+            console.log('remove initial helper text')
+        }
+    })
+}
+
+allInputs.forEach((input)=>{
+   helperTextCheck(input)
+})
+
+allUrlInputs.forEach((input)=>{
+   helperTextCheck(input)
 })
