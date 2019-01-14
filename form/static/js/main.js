c98998443959e7ce78dd108035a4ee007a84d13f
Rye-Guy <ryanracioppo95@gmail.com>
2019-01-14 15:33:32 -0500

    fixed front end disaster, smooth af now :P

--- form/static/js/main.js
+++ form/static/js/main.js
@@ -12,81 +12,20 @@ function parsePositioning(elem){
     num = parseInt(numtoParse.substring(0, numtoParse.indexOf('p')))
     return num
 }
-
-
 checkboxes.forEach((elem) =>{
     console.log(elem.checked)
     elem.addEventListener('click', ()=>{
         targetElem = elem.parentNode.parentNode.childNodes[3]
-        currentHeight = 0
-        changedHeight = 0
-        topPositioning = 0
-        numOfCheckedBoxes = 0
-        checkboxes.forEach((checkbox)=>{
-            if(checkbox.checked){
-               numOfCheckedBoxes++
-            }
-         })
-        if(elem.checked == false){
-
-                console.log(numOfCheckedBoxes)
-                if(numOfCheckedBoxes == 1){
-                    console.log('hello')
-                    fairContainers.forEach((elem)=>{
-                        if(parsePositioning(elem) > 300){
-                            elem.style.top = 300 + 'px'
-                    }
-                })
-                }else if(numOfCheckedBoxes == 2){
-                    fairContainers.forEach((elem)=>{
-                        theNum = parsePositioning(elem)
-                        if(theNum == 600){
-                            elem.style.top = 300 + 'px'
-                        }else if(theNum == 900){
-                            elem.style.top = 600 + 'px'
-                        }else if(theNum == 1200){
-                            elem.style.top = 600 + 'px'
-                        }
-                    })
-                }else if(numOfCheckedBoxes >= 3){
-                   fairContainers.forEach((elem)=>{
-                        theNum = parsePositioning(elem)
-                        console.log(theNum)
-                        if(theNum == 300){
-                            return
-                        }else if(theNum == 600){
-                            elem.style.top = 300 + 'px'
-                        }else if(theNum == 900){
-                            elem.style.top = 600 + 'px'
-                        }else if(theNum == 1200){
-                            elem.style.top = 900 + 'px'
-                        }
-                })
-                }
-            value = targetElem.style.top
-            theTakeAway = parseInt(value.substring(0, value.indexOf('p'))) - 300
-            console.log(theTakeAway)
-        }else{
-            fairContainers.forEach((elem) =>{
-                topHeight = elem.style.top
-                if(topHeight.length > 0){
-                    value = parseInt(topHeight.substring(0, topHeight.indexOf('p')))
-                }else{
-                    value = 0
-                }
-                topPositioning += value
-                currentHeight +=  elem.offsetHeight
-            })
-        }
-        targetElem.style.top = currentHeight + 300 + 'px'
         checked_box_id = elem.getAttribute('id')
         clicked_box_id = document.getElementById(checked_box_id)
         checked_box_name = elem.getAttribute('value')
             if(checked_box_name === 'Toronto' && clicked_box_id.checked){
                 torontoContainer = document.getElementById('TorontoFairOptionsContainer')
                 torontoContainer.style.display = 'flex'
+                torontoContainer.style.top = 0 + 'px'
             }else if(checked_box_name === 'Toronto' && clicked_box_id.checked == false){
                 torontoContainer.style.display = 'none'
+                torontoContainer.style.top = 0 + 'px'
                 torontoOptions = document.querySelectorAll("input[name='toronto_booth_options']")
                 torontoDates = document.querySelectorAll("input[name='toronto_dates']")
                 torontoDates.forEach((input)=>{
@@ -101,6 +40,7 @@ checkboxes.forEach((elem) =>{
                 calgaryContainer.style.display = 'flex'
             }else if(checked_box_name === 'Calgary' && clicked_box_id.checked == false){
                 calgaryContainer.style.display = 'none'
+                calgaryContainer.style.top = 0 + 'px'
                 calgaryOptions = document.querySelectorAll("input[name='calgary_booth_options']")
                 calgaryDates = document.querySelectorAll("input[name='calgary_dates']")
                 calgaryDates.forEach((input)=>{
@@ -115,6 +55,7 @@ checkboxes.forEach((elem) =>{
                 edmontonContainer.style.display = 'flex'
             }else if(checked_box_name === 'Edmonton' && clicked_box_id.checked  == false){
                 edmontonContainer.style.display = 'none'
+                edmontonContainer.style.top = 0 + 'px'
                 edmontonOptions = document.querySelectorAll("input[name='edmonton_booth_options']")
                 edmontonDates = document.querySelectorAll("input[name='edmonton_dates']")
                 edmontonDates.forEach((input)=>{
@@ -128,6 +69,8 @@ checkboxes.forEach((elem) =>{
                 winnipegContainer.style.display = 'flex'
             }else if(checked_box_name === 'Winnipeg' && clicked_box_id.checked  == false){
                 winnipegContainer.style.display = 'none'
+                winnipegContainer.style.top = 0 + 'px'
+
                 winnipegOptions = document.querySelectorAll("input[name='winnipeg_booth_options']")
                 winnipegDates = document.querySelectorAll("input[name='winnipeg_dates']")
                 winnipegDates.forEach((input)=>{
@@ -137,6 +80,87 @@ checkboxes.forEach((elem) =>{
                     input.checked = false
                 })
         }
-      targetElem.style.top = currentHeight + 300 + 'px'
+        currentHeight = 0
+        changedHeight = 0
+        topPositioning = 0
+        numOfCheckedBoxes = 0
+        firstSpotTaken = false;
+        secondSpotTaken = false;
+        thirdSpotTaken = false;
+        checkboxes.forEach((checkbox)=>{
+            if(checkbox.checked){
+               numOfCheckedBoxes++
+            }
+        })
+        fairContainers.forEach((elem)=>{
+          theNum = parsePositioning(elem)
+             if(theNum === 300){
+                firstSpotTaken = true
+             }else if(theNum === 600){
+                secondSpotTaken = true
+            }else if(theNum === 900){
+                thirdSpotTaken = true
+            }
+         })
+
+        if(elem.checked == false){
+            if(numOfCheckedBoxes == 1){
+                 fairContainers.forEach((elem)=>{
+                    if(parsePositioning(elem) > 300){
+                       elem.style.top = 300 + 'px'
+                    }
+                })
+            }
+            else if(numOfCheckedBoxes == 2){
+                fairContainers.forEach((elem) =>{
+                   theNum = parsePositioning(elem)
+                   if(firstSpotTaken == true && secondSpotTaken == true){
+                      return
+                   }else if(firstSpotTaken == false && secondSpotTaken == true && theNum != 600){
+                        elem.style.top = 300 + 'px'
+                   }else if(firstSpotTaken == true && secondSpotTaken == false && theNum != 300){
+                        elem.style.top = 600 + 'px'
+                   }
+                })
+            }
+            else if(numOfCheckedBoxes >= 3){
+                   fairContainers.forEach((elem)=>{
+                        theNum = parsePositioning(elem)
+                        if(firstSpotTaken == true && secondSpotTaken == true && thirdSpotTaken == true){
+                            return
+                        }else if(firstSpotTaken == true && secondSpotTaken == true && thirdSpotTaken == false){
+                            if(theNum == 1200){
+                                elem.style.top = 900 + 'px'
+                            }
+                        }else if(firstSpotTaken == true && secondSpotTaken == false && thirdSpotTaken == true){
+                            if(theNum == 1200){
+                                elem.style.top = 600 + 'px'
+                            }
+                        }else if(firstSpotTaken == false && secondSpotTaken == true && thirdSpotTaken == true){
+                            if(theNum == 1200){
+                                elem.style.top = 300 + 'px'
+                            }
+                        }
+                })
+            }
+            value = targetElem.style.top
+            theTakeAway = parseInt(value.substring(0, value.indexOf('p'))) - 300
+            console.log(theTakeAway)
+        }else{
+            fairContainers.forEach((elem) =>{
+                topHeight = elem.style.top
+                console.log(topHeight)
+                if(topHeight.length > 0){
+                    value = parseInt(topHeight.substring(0, topHeight.indexOf('p')))
+                }else{
+                    value = 0
+                }
+                topPositioning += value
+                currentHeight +=  elem.offsetHeight
+                console.log(topPositioning)
+                console.log(currentHeight)
+                })
+            targetElem.style.top = currentHeight + 'px'
+        }
     })
 })
